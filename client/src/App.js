01fa8cbd62f1d36e9057c2e27774daf00072f40b
Kai Neuhold-Huber <kneuholdhuber@gmail.com>
2022-01-20 15:23:44 -0800

    react front end setup

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Router } from "@reach/router";
+
+// views
+
+// bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+    return (
+        <div className="App">
+            <Router>
+                
+            </Router>
+        </div>
+    );
+}
+
+export default App;
